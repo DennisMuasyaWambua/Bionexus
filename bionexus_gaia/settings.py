84b7a06459fe105ac0b17851c5f0a38501d055a3
Dennis wambua <muasyathegreat4@gmail.com>
2025-09-25 21:22:52 +0300

    bionexus-ohn92.ondigitalocean.app added to allowed hosts

--- bionexus_gaia/settings.py
+++ bionexus_gaia/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-r0!d8fn=pb6r-7%+&#!v^04lxf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1','bionexus-ohn92.ondigitalocean.app').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,bionexus-ohn92.ondigitalocean.app').split(',')
 
 
 # Application definition
